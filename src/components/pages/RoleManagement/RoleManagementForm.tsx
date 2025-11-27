9a27eb9e253f02675a11436543ea421c61d12484
Arjun Jhukal <jhukal9@gmail.com>
2025-11-27 15:25:05 +0545

    cleared the user code base

--- src/components/pages/RoleManagement/RoleManagementForm.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function RoleManagementForm() {
-    return (
-        <h1>Role Management Form</h1>
-    )
-}
--- src/components/pages/RoleManagement/allRoles/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AllRoles() {
-    return (
-        <div>AllRoles</div>
-    )
-}
